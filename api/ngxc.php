bba5774d9f7b1c3fea68297e35a55df4fcdc312a
Chris <23177238+vertig0ne@users.noreply.github.com>
2018-02-14 20:22:21 +0000

    Create ngxc.php

---
+++ api/ngxc.php
@@ -0,0 +1,31 @@
+<?php
+if(isset($_POST['data']['plugin'])){
+    switch ($_POST['data']['plugin']) {
+        case 'NGXC/settings/get':
+            if(qualifyRequest(1)){
+                $result['status'] = 'success';
+                $result['statusText'] = 'success';
+                $result['data'] = NGXCGetSettings();
+            }else{
+                $result['status'] = 'error';
+                $result['statusText'] = 'API/Token invalid or not set';
+                $result['data'] = null;
+            }
+            break;
+        case 'NGXC/settings/save':
+            if(qualifyRequest(1)){
+		NGXCWriteConfig();
+                $result['status'] = 'success';
+                $result['statusText'] = 'success';
+                $result['data'] = true;
+            }else{
+                $result['status'] = 'error';
+                $result['statusText'] = 'API/Token invalid or not set';
+                $result['data'] = null;
+            }
+            break;
+        default:
+            //DO NOTHING!!
+            break;
+    }
+}
