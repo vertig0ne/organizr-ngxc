d970fd8f4cb62b5ab6bbcb978903200eed16fc1f
vertig0ne <23177238+vertig0ne@users.noreply.github.com>
2018-02-20 21:16:54 +0000

    Fix up install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,10 @@
-##!/usr/bin/with-contenv bash
+#!/bin/sh
 
-if [ ! -z "$ORGANIZR_PATH" ]; then
-    ORGANIZR_PATH=/config/www
-fi
+ORGANIZR_PATH=/config/www
 
 echo "Organizr-ngxc Plugin Installer - v0.1"
-mkdir -p '${ORGANIZR_PATH}/api/plugins/ngxc'
-cp -R ngxc/* ${ORGANIZR_PATH}/api/plugins/ngxc
-cp api/ngxc.php ${ORGANIZR_PATH}/api/plguins/api/ngxc.php
-cp config/ngxc.php ${ORGANIZR_PATH}/api/plguins/config/ngxc.php
-cp js/ngxc.js ${ORGANIZR_PATH}/api/plguins/js/ngxc.js
-cp images/ngxc.png ${ORGANIZR_PATH}/plugins/images/ngxc.png
-cp ngxc.php ${ORGANIZR_PATH}/api/plguins/ngxc.php
+cp ./api/ngxc.php $ORGANIZR_PATH/api/plugins/api/
+cp ./js/ngxc.js $ORGANIZR_PATH/api/plugins/js/
+cp ./images/ngxc.png $ORGANIZR_PATH/plugins/images/
+cp ./ngxc.php $ORGANIZR_PATH/api/plugins/
+echo "Complete"
